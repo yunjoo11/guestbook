87a1be3d7467ac35205dd4f9270cb5129b9fbe62
devops <devops@jadecross.com>
2025-03-10 07:14:57 +0000

    [ADD] TestCase - PostTest.java

---
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -0,0 +1,10 @@
+package com.jadecross.guestbook;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+class PostTest {
+  @Test
+  void testSetName() {
+    Post post = new Post("TESTER", "2022-04-12 01:01:48", "Happy Wedding"); post.setName("DEVOPS");
+    assertEquals("DEVOPS", post.getName());
+  }
+}
